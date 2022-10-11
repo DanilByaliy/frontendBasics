e61437efe9a244b7106c3b19ca2e9e56503cf40f
DanilByaliy <byaliy03@gmail.com>
2022-10-11 23:07:14 +0300

    Complete the first task of the fourth laboratory work

--- lab4/index.js
+++ lab4/index.js
@@ -1,1 +1,19 @@
-"use strict"
+'use strict'
+
+let element1 = document.getElementById("list");
+
+element1.addEventListener('click', () => {
+    element1.style.backgroundColor = '#' + (Math.random().toString(16)
+     + '000000').substring(2,8).toUpperCase();
+     element1.style.color = '#' + (Math.random().toString(16)
+     + '000000').substring(2,8).toUpperCase()
+});
+
+let element2 = document.querySelector('.text');
+
+element2.addEventListener('click', () => {
+    element2.style.backgroundColor = '#' + (Math.random().toString(16)
+     + '000000').substring(2,8).toUpperCase();
+     element2.style.color = '#' + (Math.random().toString(16)
+     + '000000').substring(2,8).toUpperCase()
+});

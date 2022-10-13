b9d2a3f98af14bd7400814a3dad5df7563bcda9a
DanilByaliy <byaliy03@gmail.com>
2022-10-13 19:43:01 +0300

    Add the function of reducing and enlarging the picture

--- lab4/index.js
+++ lab4/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-let element1 = document.getElementById("list");
+const element1 = document.getElementById("list");
 
 element1.addEventListener('click', () => {
     element1.style.backgroundColor = '#' + (Math.random().toString(16)
@@ -9,11 +9,25 @@ element1.addEventListener('click', () => {
      + '000000').substring(2,8).toUpperCase()
 });
 
-let element2 = document.querySelector('.text');
+const element2 = document.querySelector('.text');
 
 element2.addEventListener('click', () => {
     element2.style.backgroundColor = '#' + (Math.random().toString(16)
      + '000000').substring(2,8).toUpperCase();
      element2.style.color = '#' + (Math.random().toString(16)
      + '000000').substring(2,8).toUpperCase()
+});
+
+const image = document.querySelector('.image');
+const buttonIncrease = document.querySelector('.increase');
+const buttonReduce = document.querySelector('.reduce');
+
+buttonIncrease.addEventListener('click', () => {
+    const width = image.width;
+    image.style.width = width + 50 + 'px';
+});
+
+buttonReduce.addEventListener('click', () => {
+    const width = image.width;
+    image.style.width = width - 50 + 'px';
 });

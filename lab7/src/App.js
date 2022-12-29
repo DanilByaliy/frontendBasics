f992b120f4821b6eb268633cb98f839a9c33d835
DanilByaliy <byaliy03@gmail.com>
2022-12-29 21:49:44 +0200

    Add the initial state of the application

---
+++ lab7/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Task1 from './components/Task1';
+import Task2 from './components/Task2';
+
+function App() {
+  return (
+    <div className="App">
+     <Task1 />
+     <Task2 />
+    </div>
+  );
+}
+
+export default App;
